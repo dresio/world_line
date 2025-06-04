35cbe619e8ce85e410dfb0b91a92f8fbd37b59d7
Daniel Resio <dresiomob@gmail.com>
2025-06-03 21:56:52 -0400

    Basic character controller. Added new player model from blender

--- src/controls.rs
+++ src/controls.rs
@@ -1,24 +1,24 @@
 use bevy::prelude::*;
 use bevy_tnua::prelude::*;
 
-
 #[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
 pub struct Controller;
 
-impl Plugin for Controller
-{
+impl Plugin for Controller {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, apply_movement);
     }
 }
 
-fn apply_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut TnuaController>) {
-    
+fn apply_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut TnuaController>,
+) {
     let Ok(mut controller) = query.single_mut() else {
         return;
     };
-    
+
     //pressed
     //just_pressed
     //just_released
@@ -26,22 +26,21 @@ fn apply_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<&m
     let mut direction = Vec3::ZERO;
 
     if keyboard_input.pressed(KeyCode::KeyA) {
-        direction.x -= 1.0;
+        direction.z += 1.0;
     }
     if keyboard_input.pressed(KeyCode::KeyD) {
-        direction.x += 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::KeyS) {
         direction.z -= 1.0;
     }
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        direction.x += 1.0;
+    }
     if keyboard_input.pressed(KeyCode::KeyW) {
-        direction.z += 1.0;
+        direction.x -= 1.0;
     }
 
     controller.basis(TnuaBuiltinWalk {
-        desired_velocity: direction * 20.0,
-        float_height: 2.2,
+        desired_velocity: direction * 50.0,
+        float_height: 18.0,
         ..Default::default()
     });
-
 }
--- src/player.rs
+++ src/player.rs
@@ -1,16 +1,14 @@
-use bevy::prelude::*;
 use avian3d::prelude::*;
+use bevy::prelude::*;
 use bevy_tnua::prelude::*;
-
 
 #[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
 pub struct Player;
 
-impl Plugin for Player
-{
+impl Plugin for Player {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (spawn_player));
+        app.add_systems(Startup, spawn_player);
     }
 }
 
@@ -23,20 +21,19 @@ pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
                 // Change this to your exported gltf file
                 GltfAssetLabel::Scene(1).from_asset("TestScene.glb"),
             )),
-            Transform::from_xyz(0.0, 10.0, 0.0),
+            Transform::from_xyz(0.0, 20.0, 0.0),
             TnuaController::default(),
             RigidBody::Dynamic,
-            ColliderConstructor::Cuboid {
-                x_length: 1.0,
-                y_length: 1.0,
-                z_length: 1.0,
-            }, //TODO: Fix collider similar to what is used to get world collider, but need to also fix rotations about all but yaw
+            ColliderConstructor::Cylinder {
+                radius: (10.0),
+                height: (30.0),
+            },
         ))
         //Add camera as child for camera position
         .with_children(|parent| {
             parent.spawn((
                 Camera3d::default(),
-                Transform::from_xyz(3.0, 5.0, 3.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+                Transform::from_xyz(50.0, 50.0, 0.0).looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
             ));
         });
 }
